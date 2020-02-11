0dbc2c430e4c0b1e70ec8744866ced25e69aeb5d
yks095 <rltjr219@naver.com>
2020-02-11 20:20:03 +0900

    Annotation 기반으로 개선

--- src/main/java/com/kiseok/springboot/web/IndexController.java
+++ src/main/java/com/kiseok/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.kiseok.springboot.web;
 
+import com.kiseok.springboot.config.auth.LoginUser;
 import com.kiseok.springboot.config.auth.dto.SessionUser;
 import com.kiseok.springboot.service.posts.PostsService;
 import com.kiseok.springboot.web.dto.PostsResponseDto;
@@ -16,12 +17,10 @@ import javax.servlet.http.HttpSession;
 public class IndexController {
 
     private final PostsService postsService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model)   {
+    public String index(Model model, @LoginUser SessionUser user)   {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if(user != null)    {
             model.addAttribute("userName", user.getName());
         }
