dddac25af992f5c0668972d62d81c38e0c5a6a92
yks095 <rltjr219@naver.com>
2020-02-11 19:24:44 +0900

    구글 소셜 로그인 구현

---
+++ src/main/java/com/kiseok/springboot/web/domain/user/UserRepository.java
@@ -0,0 +1,8 @@
+package com.kiseok.springboot.web.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
